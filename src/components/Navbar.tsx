d0ee9a6850378bd8e0b1a9244bab4aa3dff534a6
Shark-44 <joanny.bernardeau@gmail.com>
2025-01-21 10:05:27 +0100

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,36 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <div
-      className="fixed top-0 left-0 w-full z-50 bg-opacity-70 bg-gray-800"
-      style={{ backdropFilter: "blur(10px)" }} // Optionnel : ajoute un effet de flou pour une meilleure lisibilité
-    >
+    <div className="fixed top-0 left-0 w-full z-50 bg-opacity-70 bg-gray-800" style={{ backdropFilter: "blur(10px)" }}>
       <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
         <li>
-          <a
-            href="/"
+          <Link
+            to="/"
             className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-            aria-current="page"
           >
             Home
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/One"
+          <Link
+            to="/One"
             className="block py-2 px-3 text-gray-200 hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-400"
           >
             Mes projets
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/Contact"
+          <Link
+            to="/Contact"
             className="block py-2 px-3 text-gray-200 hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-400"
           >
             Contact
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
   );
 }
 
-export default Navbar;
+export default Navbar
